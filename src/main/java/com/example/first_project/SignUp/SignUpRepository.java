84e69ed603c6538355cab8d2adff85dfb8ba8865
dgsw21 <dgsw21@dgsw21ui-MacBookPro.local>
2026-06-14 13:45:47 +0900

    모르겠다.

--- src/main/java/com/example/first_project/SignUp/SignUpRepository.java
+++ src/main/java/com/example/first_project/SignUp/SignUpRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface SignUpRepository extends JpaRepository<SignUp, Long> { //
 
     SignUp findByNickName(String nickName); // 이름을 찾아오는 함수 근데 이건 생성자도 아니고 변수에() 이거 붙이는 거 모르겠음
+
     SignUp findByPhoneNumber(String phoneNumber); // 전화번호를 찾아오는 함
-    SignUp findByPassword(String password);
 }
 
 
